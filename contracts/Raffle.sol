bac26ae074f308de44995b733d95b9b387f367a6
Will Stansill <willstansill@gmail.com>
2022-11-03 13:39:10 -0600

    second commit

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -18,7 +18,7 @@ error Raffle__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
 
 /**@title A sample Raffle Contract
- * @author Patrick Collins
+ * @author Will Stansill
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
